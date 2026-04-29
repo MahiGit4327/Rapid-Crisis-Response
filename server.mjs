57d4dd3f13f453065623c1c822b382d0c93588b6
MahiGit4327 <patelmahi4327@gmail.com>
2026-04-29 22:20:48 +0530

    fix vercel deployment

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const host = "localhost";
+const host = "0.0.0.0";
 const port = parseInt(process.env.PORT || "3000", 10);
 
 const app = next({ dev, hostname: host, port });
